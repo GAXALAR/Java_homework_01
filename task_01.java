e92bb4f5681b404bf061b439c1126757bf8bba09
GAXALAR <ay.kosyrev@gmail.com>
2023-04-19 22:44:34 +0300

    дз №1

--- task_01.java
+++ task_01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class task_01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
